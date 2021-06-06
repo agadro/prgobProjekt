10805c0bf52e0eeb5eb48890a3a40b326fc8eba8
dominikp7 <dominikpi7@o2.pl>
2021-06-07 00:21:05 +0200

    update

--- test_beaufort.py
+++ test_beaufort.py
@@ -1,8 +1,24 @@
 from unittest import TestCase
-from main import szyfruj_lub_deszyfruj
-
-class Test(TestCase):
+from main import Beaufort
 
 
+class TestBeaufort(TestCase):
+    def setUp(self):
+        self.beaufort = Beaufort()
+
+
+class TestInit(TestBeaufort):
+    def test_initial_key(self):
+        self.assertEqual(self.beaufort.key, 'C')
+
+
+class TestSzyfruj(TestBeaufort):
     def test_szyfruj_lub_deszyfruj(self):
-        self.assertEqual('test',szyfruj_lub_deszyfruj('jykj'))
+        test = self.beaufort.szyfruj_lub_deszyfruj('test')
+        self.assertEqual(test,'JYKJ')
+
+    def test_szyfruj_lub_deszyfruj2(self):
+        test = self.beaufort.szyfruj_lub_deszyfruj('test')
+        test2 = self.beaufort.szyfruj_lub_deszyfruj(test)
+        self.assertEqual(self.beaufort.szyfruj_lub_deszyfruj(test2),test)
+
